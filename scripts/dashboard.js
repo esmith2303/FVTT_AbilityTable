d89171ea7978e9a43968ed2452c1f651ffe6df74
esmith2303 <elian2006@hotmail.co.uk>
2025-10-16 21:46:13 +0100

    Adding rogar

--- scripts/dashboard.js
+++ scripts/dashboard.js
@@ -37,7 +37,7 @@ export class StatsDashboard extends Application {
       Copper:0
     };
     let totalpartygold = 0;
-    const validSubstrings = ["karl", "gobb", "odd", "alisker"];
+    const validSubstrings = ["karl", "gobb", "odd", "alisker", "rogar"];
     const players = game.actors.filter(a =>
       a.type === "character" &&
       validSubstrings.some(sub => a.name.toLowerCase().includes(sub))
